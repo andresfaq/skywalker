9937df023a3b4928abcb2107802110ea26544b43
Andres Artunduaga <andres.artunduaga@outlook.com>
2017-02-13 10:11:40 -0500

    US3: A basic django structure was created

---
+++ skywalker/skywalker/settings/prod.py
@@ -0,0 +1,14 @@
+from .base import *
+
+DEBUG = False
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'skywalker',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
