664638f24fe95b32c3a50074f19fafd8df27c688
Andres Artunduaga <andres.artunduaga@outlook.com>
2017-02-14 04:54:01 +0000

    base.py edited online with Bitbucket

--- skywalker/skywalker/settings/base.py
+++ skywalker/skywalker/settings/base.py
@@ -48,7 +48,7 @@ TENANT_APPS = [
 
 ]
 
-INSTALLED_APPS = list(SHARED_APPS) + [app for app in TENANT_APPS if app not in SHARED_APPS]
+INSTALLED_APPS = list(set(TENANT_APPS + SHARED_APPS))
 
 MIDDLEWARE = [
     'tenant_schemas.middleware.DefaultTenantMiddleware',
