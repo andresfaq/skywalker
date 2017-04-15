5d989688064bb768e6544004acbeb47f9bdeb896
Jeffer Mendoza <jefferson.mendoza@correounivalle.edu>
2017-04-15 18:36:01 -0500

    add views for crud pizza

--- skywalker/pizza/views.py
+++ skywalker/pizza/views.py
@@ -9,18 +9,20 @@ from django.views.generic import (
 
 from baseapp.models import (
     Ingredient,
-    PizzaBase
+    PizzaBase,
+    Pizza
 )
 
 from baseapp.mixin import TitleContentPageMixin
 
 from .forms import (
     IngredientForm,
-    PizzaBaseForm
+    PizzaBaseForm,
+    PizzaForm
 )
 
 
-
+# ENTITY INGREDIENT
 class IngredientList(TitleContentPageMixin, ListView):
     model = Ingredient
     template_name = "ingredient/list.html"
@@ -65,7 +67,7 @@ class IngredientDelete(TitleContentPageMixin, DeleteView):
     template_name = "ingredient/confirm_delete.html"
     title_content = "Delete Ingredient"
 
-
+# ENTITY PIZZA BASE
 class PizzaBaseList(TitleContentPageMixin,ListView):
     model = PizzaBase
     template_name = "pizzaBase/list.html"
@@ -109,3 +111,48 @@ class PizzaBaseDelete(TitleContentPageMixin, DeleteView):
     success_url = reverse_lazy('pizza:pizzabase_list')
     template_name = "pizzaBase/confirm_delete.html"
     title_content = "Delete Base Pizza"
+
+# ENTITY PIZZA
+class PizzaList(TitleContentPageMixin,ListView):
+    model = Pizza
+    template_name = "pizza/list.html"
+    title_content = "Pizzas"
+
+class PizzaDetail(TitleContentPageMixin, DetailView):
+    model = Pizza
+    template_name = "pizza/detail.html"
+    title_content = "Detail Pizza"
+
+class PizzaCreation(TitleContentPageMixin, SuccessMessageMixin,CreateView):
+    model = Pizza
+    success_url = reverse_lazy('pizza:pizza_list')
+    form_class = PizzaForm
+    template_name = "object/object_form.html"
+    success_message = "Pizza create successfully"
+    title_content = "New Pizza"
+
+    def get_context_data(self, **kwargs):
+        context = super(PizzaCreation, self).get_context_data(**kwargs)
+        context['form_title'] = "Create a pizza"
+        return context
+
+
+class PizzaUpdate(TitleContentPageMixin, SuccessMessageMixin,UpdateView):
+    model = Pizza
+    success_url = reverse_lazy('pizza:pizza_list')
+    form_class = PizzaForm
+    template_name = "object/object_form.html"
+    success_message = "Pizza edit successfully"
+    title_content = "Update Pizza"
+
+    def get_context_data(self, **kwargs):
+        context = super(PizzaUpdate, self).get_context_data(**kwargs)
+        context['form_title'] = "Create a pizza"
+        return context
+
+
+class PizzaDelete(TitleContentPageMixin, DeleteView):
+    model = Pizza
+    success_url = reverse_lazy('pizza:pizza_list')
+    template_name = "pizza/confirm_delete.html"
+    title_content = "Delete Pizza"
