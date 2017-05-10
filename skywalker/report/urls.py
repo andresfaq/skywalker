b3ac374375c9318be1205204bb75f56801430d41
julianrico1994 <julianrico18@gmail.com>
2017-05-10 17:22:23 -0500

    US45: Consulta los ingredintes mas vendidos en la base de datos, limpieza de codigo

--- skywalker/report/urls.py
+++ skywalker/report/urls.py
@@ -6,5 +6,8 @@ from report.views import (
 
 
 urlpatterns = [
-    url(r'^most_sold_ingredients/$', most_sold_ingredients.as_view(), name='most_sold_ingredients'),
+    #url(r'^most_sold_ingredients/$', most_sold_ingredients.as_view(), name='most_sold_ingredients'),
+    url(r'^most_sold_ingredients/$', most_sold_ingredients, name='most_sold_ingredients'),
+    #url(r'^most_sold_ingredients_2/(?P<datos>.*)/(?P<datos_2>.*)$',most_sold_ingredients_2,name='most_sold_ingredients_2'),
+
 ]
--- skywalker/report/views.py
+++ skywalker/report/views.py
@@ -1,5 +1,7 @@
 from django.contrib.messages.views import SuccessMessageMixin
 from django.core.urlresolvers import reverse_lazy
+from django.db.models import Sum
+from django.shortcuts import render
 from django.views.generic import (
     ListView,
     DetailView,
@@ -9,6 +11,9 @@ from django.views.generic import (
 
 from baseapp.models import (
     Ingredient,
+    Extra,
+    Order,
+    Sale,
     PizzaBase,
     Pizza
 )
@@ -16,9 +21,40 @@ from baseapp.models import (
 from baseapp.mixin import TitleContentPageMixin
 
 # REPORT
-class most_sold_ingredients(TitleContentPageMixin, ListView):
+def most_sold_ingredients(request):
     model = Ingredient
-    var1 = "contexto variable julian"
     template_name = "reports/most_sold_ingredients.html"
     title_content = "Most Sold Extra Ingredients"
+
+    consulta = Extra.objects.values('ingredient__name')\
+        .annotate(ingredient_amount=Sum('amount'))\
+        .order_by('ingredient')
+
+    ctx = {'consulta': consulta}
+    return render(request, template_name, ctx)
+
+
+def llenarDB ():
+    #list_admitidos = lista_admitidos.objects.filter(carrera=carrera).order_by('-puntaje')[:cupos]
+    #john = Author.objects.create(name="John")
+    extra      = Extra()
+    ingredient = Ingredient()
+    order      = Order ()
+    #pizzabase1  = PizzaBase.objects.get (name = "pizza1")
+
+    #order.pizza       = Pizza.objects.get      (pk = 1)
+    #order.ingredients.add (Ingredient.objects.get (pk = 1).id)
+    #order.note        = "note1"
+
+    ingredient1 = Ingredient.objects.get (pk = 2)
+    order1 = Order.objects.create (pizza = Pizza.objects.get(pk = 2), note = "note2" )
+    extra1 = Extra.objects.create (order      = order1, ingredient = ingredient1, amount     = 11 )
+
+    #print ("extra1: "+ str (extra1.amount))
+
+    #extra.order      = order1;
+    #extra.ingredient = ingredient1;
+    #extra.amount     = 2
+
+
 
