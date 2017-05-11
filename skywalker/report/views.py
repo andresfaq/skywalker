a5591e9e4c4829c74588afc9003d1b58adaba7dc
julianrico1994 <julianrico18@gmail.com>
2017-05-10 19:02:21 -0500

    US46: pequenos cambios

--- skywalker/report/views.py
+++ skywalker/report/views.py
@@ -49,7 +49,27 @@ def most_sold_ingredients(request):
     return render(request, template_name, ctx)
 
 
+def llenarDB ():
+    #list_admitidos = lista_admitidos.objects.filter(carrera=carrera).order_by('-puntaje')[:cupos]
+    #john = Author.objects.create(name="John")
+    extra      = Extra()
+    ingredient = Ingredient()
+    order      = Order ()
+    #pizzabase1  = PizzaBase.objects.get (name = "pizza1")
 
+    #order.pizza       = Pizza.objects.get      (pk = 1)
+    #order.ingredients.add (Ingredient.objects.get (pk = 1).id)
+    #order.note        = "note1"
+
+    ingredient1 = Ingredient.objects.get (pk = 2)
+    order1 = Order.objects.create (pizza = Pizza.objects.get(pk = 2), note = "note2" )
+    extra1 = Extra.objects.create (order      = order1, ingredient = ingredient1, amount     = 11 )
+
+    #print ("extra1: "+ str (extra1.amount))
+
+    #extra.order      = order1;
+    #extra.ingredient = ingredient1;
+    #extra.amount     = 2
 
 
 
