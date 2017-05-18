db70ec69d8b7e184175f50ca97c834ddf8be0ad4
Andres Artunduaga <andres.artunduaga@outlook.com>
2017-05-18 06:07:06 +0000

    creating db

--- utilities/resetdb-ci.sh
+++ utilities/resetdb-ci.sh
@@ -1,6 +1,5 @@
 # Reiniciador de la base de datos
 # Reemplazen el nombre de usuario
 # Ejecuten haciendo [sh resetdb.sh] en consola
-sudo -u ubuntu psql -c 'DROP DATABASE circleci;'
 sudo -u ubuntu psql -c 'CREATE DATABASE circleci WITH OWNER ubuntu;'
-sudo -u ubuntu psql -c 'GRANT ALL PRIVILEGES ON DATABASE skywalker TO ubuntu;'
+sudo -u ubuntu psql -c 'GRANT ALL PRIVILEGES ON DATABASE circleci TO ubuntu;'
