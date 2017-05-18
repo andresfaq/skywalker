0041730c75eec1b3fec4bdeb71f8f38bc9000aa6
Andres Artunduaga <andres.artunduaga@outlook.com>
2017-05-18 08:17:24 +0000

    test file

--- skywalker/baseapp/factories.py
+++ skywalker/baseapp/factories.py
@@ -6,38 +6,6 @@ from baseapp import models
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
 
-# # User Factory
-# class UserFactory(factory.django.DjangoModelFactory):
-#     class Meta:
-#         model = User
-
-#     username = factory.Sequence(lambda n: "Usuario%03d" % n)
-#     first_name = factory.Faker('first_name')
-#     last_name = factory.Faker('last_name')
-#     password = make_password('Usuario')
-#     is_active = True
-#     dni = factory.fuzzy.FuzzyInteger(10000000, 999999999)
-#     address = factory.Faker('address')
-#     email = factory.Faker('email')
-#     birth_date = factory.fuzzy.FuzzyDate(datetime.date(1950, 1, 1), datetime.date(2000, 12, 31))
-#     cellphone = factory.fuzzy.FuzzyInteger(1000000, 9999999)
-#     phone = factory.fuzzy.FuzzyInteger(100000000, 999999999)
-
-# # Employee Factory
-# class EmployeeFactory(UserFactory):
-#     class Meta:
-#         model = models.Employee
-
-#     username = factory.Sequence(lambda n: "Employee%03d" % n)
-#     salary = factory.fuzzy.FuzzyInteger(10000000, 999999999)
-#     employee_type = factory.fuzzy.FuzzyChoice(['Employee', 'Administrator'])
-
-# # CLient Factory
-# class ClientFactory(UserFactory):
-#     class Meta:
-#         model = models.Client
-
-#     username = factory.Sequence(lambda n: "Client%03d" % n)
 
 # Employee Factory
 class IngredientFactory(factory.Factory):
