5dd08ec877a015d4588e34bcc821d7d63fcf14fa
Andres Artunduaga <andres.artunduaga@outlook.com>
2017-05-18 06:49:26 +0000

    modifying stuff

--- skywalker/skywalker/settings/dev_ci.py
+++ skywalker/skywalker/settings/dev_ci.py
@@ -12,20 +12,3 @@ DATABASES = {
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                'skywalker.middleware.tenant_context_processors.tenant_template_style',
-            ],
-        },
-    },
-]
