72352cac59655a5ec191735c256a4ccb9388949b
jeffer-mendoza <jefferson.mendoza@correounivalle.edu.co>
2017-05-24 23:53:03 -0500

    se guarda la orden que crea el cliente, se eliminan relaciones para hacer mas simple el modelo de datos

--- skywalker/report/urls.py
+++ skywalker/report/urls.py
@@ -1,12 +1,10 @@
 from django.conf.urls import url
 
 from report.views import (
-    most_sold_ingredients,
     most_sold_pizzas,
 )
 
 
 urlpatterns = [
-    url(r'^most_sold_ingredients/$', most_sold_ingredients, name='most_sold_ingredients'),
     url(r'^most_sold_pizzas/$', most_sold_pizzas, name='most_sold_pizzas'),
 ]
