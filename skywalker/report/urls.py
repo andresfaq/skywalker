3a8fff7e84b498749a8d7ed589e5d1ef83a8140f
julianrico1994 <julianrico18@gmail.com>
2017-06-20 21:43:14 -0500

    US46: ingredientes y pizzas mas vendidas

--- skywalker/report/urls.py
+++ skywalker/report/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 
 from report.views import (
+    most_sold_ingredients,
     most_sold_pizzas,
 )
 
 
 urlpatterns = [
-    url(r'^most_sold_pizzas/$', most_sold_pizzas, name='most_sold_pizzas'),
+    url(r'^most_sold_ingredients/$', most_sold_ingredients, name='most_sold_ingredients'),
+    url(r'^most_sold_pizzas/$',      most_sold_pizzas,      name='most_sold_pizzas'),
 ]
--- skywalker/report/views.py
+++ skywalker/report/views.py
@@ -12,6 +12,8 @@ from django.views.generic import (
 
 from baseapp.models import (
     Ingredient,
+    # Extra,
+    Order,
     Sale,
     PizzaBase,
     Pizza
@@ -22,6 +24,10 @@ from baseapp.mixin import TitleContentPageMixin
 def most_sold_pizzas (request):
     template_name = "reports/most_sold_pizzas.html"
 
+    # la consulta hace join de: order con pizza con pizza_base los
+    # __ significa join o seleccionar una columna
+    # annotate son funciones extra que se le puede meter a la consulta como : Sum y Count
+    # -count_pizzabase: ordenar de forma descendente
     consulta = Order.objects.values('pizza__pizza_base__name')\
         .annotate(count_pizzabase = Count('pizza__pizza_base__name'))\
         .order_by('-count_pizzabase')
@@ -29,6 +35,24 @@ def most_sold_pizzas (request):
     ctx = {'consulta': consulta}
     return render(request, template_name, ctx)
 
+# REPORT
+def most_sold_ingredients(request):
+    model = Ingredient
+    template_name = "reports/most_sold_ingredients.html"
+    title_content = "Most Sold Extra Ingredients"
+    """
+    consulta = Extra.objects.values('ingredient__name')\
+        .annotate(ingredient_amount=Sum('amount'))\
+        .order_by('ingredient')
+        """
+    """
+    consulta = Extra.objects.values('ingredient__name')\
+        .annotate(ingredient_amount=Sum('amount'))\
+        .order_by('-ingredient_amount')"""
+    consulta = ""
+
+    ctx = {'consulta': consulta}
+    return render(request, template_name, ctx)
 
 
 
