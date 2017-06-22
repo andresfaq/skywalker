4dfb00d3c1e4846f461b383bbac5b9f6fe08aa89
jeffer-mendoza <jefferson.mendoza@correounivalle.edu.co>
2017-06-22 13:57:46 +0000

    Agrega configuración para la base de datos que será usada para las pruebas

---
+++ skywalker/skywalker/settings/test-heroku-database.py
@@ -0,0 +1,12 @@
+from .base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'tenant_schemas.postgresql_backend',
+        'NAME': 'd1suo5thn7t1i',
+        'USER': 'ohwklrmvbovywo',
+        'PASSWORD': '915c590f5fba42118f437ccec2399042e1b55923a911702cb445648b5fb4a1b9',
+        'HOST': 'ec2-54-225-71-119.compute-1.amazonaws.com',
+        'PORT': '5432',
+    }
+}
